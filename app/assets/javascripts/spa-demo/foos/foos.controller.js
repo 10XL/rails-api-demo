a76a546b2155be9044ed38804d63c37dd2fa4715
10xl <urnotuurmi@gmail.com>
2017-03-30 22:15:35 -0400

    sample directive and template page

--- app/assets/javascripts/spa-demo/foos/foos.controller.js
+++ app/assets/javascripts/spa-demo/foos/foos.controller.js
@@ -5,7 +5,7 @@
     .module('spa-demo.foos')
     .controller('spa-demo.foos.FoosController', FoosController);
 
-  FoosController.$inject = ['spa-demo.foos.Foos'];
+  FoosController.$inject = ['spa-demo.foos.Foo'];
 
   function FoosController(Foo) {
     var vm = this;
