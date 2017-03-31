b17969d8121039248cf70ff81555d96608001fe7
10xl <urnotuurmi@gmail.com>
2017-03-30 21:36:29 -0400

    sample resource crud controller skeleton

---
+++ app/assets/javascripts/spa-demo/foos/foos.controller.js
@@ -0,0 +1,45 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('spa-demo.foos')
+    .controller('spa-demo.foos.FoosController', FoosController);
+
+  FoosController.$inject = ['spa-demo.foos.Foos'];
+
+  function FoosController(Foo) {
+    var vm = this;
+    vm.foos;
+    vm.foo;
+
+    activate();
+    return;
+    ////////////////
+    function activate() {
+      newFoo();
+    }
+
+    function newFoo() {
+      vm.foo = new Foo();
+    }
+    function handleError(res) {
+      console.log(res);
+    }
+    function edit(object, index) {
+
+    }
+    function create() {
+
+    }
+    function update() {
+
+    }
+    function remove() {
+
+    }
+    function removeElement(elements, element) {
+
+    }
+
+  }
+})();
