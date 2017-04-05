08788e02ef3e78c92b420def28a3728fb0f7514f
10xl <urnotuurmi@gmail.com>
2017-04-05 02:12:33 -0400

    create resource instance

--- app/assets/javascripts/spa-demo/foos/foos.controller.js
+++ app/assets/javascripts/spa-demo/foos/foos.controller.js
@@ -11,6 +11,10 @@
     var vm = this;
     vm.foos;
     vm.foo;
+    vm.edit = edit;
+    vm.create = create;
+    vm.update = update;
+    vm.remove = remove;
 
     activate();
     return;
@@ -23,21 +27,33 @@
     function newFoo() {
       vm.foo = new Foo();
     }
+
     function handleError(res) {
       console.log(res);
     }
+
     function edit(object, index) {
 
     }
-    function create() {
 
+    function create() {
+      //console.log("creating foo", vm.foo);
+      vm.foo.$save()
+        .then(function(res) {
+          console.log(res);
+          vm.foos.push(vm.foo);
+          newFoo();
+        }).catch(handleError);
     }
+
     function update() {
 
     }
+
     function remove() {
 
     }
+
     function removeElement(elements, element) {
 
     }
