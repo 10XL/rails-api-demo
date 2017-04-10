6ea26067bad4d98d7ad21c48abae307dc97e9cc4
10xl <urnotuurmi@gmail.com>
2017-04-10 02:09:37 -0400

    added cors heroku origin

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module CapstoneDemoapp
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins '*'
+        origins 'https://capstone-module1-demo.herokuapp.com'
 
         resource '/api/*',
           :headers => :any,
