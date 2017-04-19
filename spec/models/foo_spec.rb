63c73913d46121ef07b3e88027e37db2e2b354c7
10xl <urnotuurmi@gmail.com>
2017-04-19 18:04:29 -0400

    spec: thinning examples

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -2,14 +2,19 @@ require 'rails_helper'
 
 describe Foo, type: :model do
 
-  context "created Foo" do
+  context "created Foo (let)" do
     let(:foo) { Foo.create(:name=>"test") }
-    
-    it "will be persisted, have a name, and be found" do
-      expect(foo).to be_persisted
-      expect(foo.name).to eq("test")
-      expect(Foo.find(foo.id)).to_not be_nil
-    end
+
+    it { expect(foo).to be_persisted }
+    it { expect(foo.name).to eq("test") }
+    it { expect(Foo.find(foo.id)).to_not be_nil }
   end
 
+  context "created Foo (subject)" do
+    subject(:foo) { Foo.create(:name=>"test") }
+
+    it { is_expected.to be_persisted }
+    it { expect(subject.name).to eq("test") }
+    it { expect(Foo.find(subject.id)).to_not be_nil }
+  end
 end
