a49713d828b930e5b4a3abc294111a1e73f2c03f
10xl <urnotuurmi@gmail.com>
2017-04-19 18:14:05 -0400

    spec: pending examples, lazy/eager let blocks

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe Foo, type: :model do
-
   context "created Foo (let)" do
     let(:foo) { Foo.create(:name=>"test") }
 
@@ -16,5 +15,15 @@ describe Foo, type: :model do
     it { is_expected.to be_persisted }
     it { expect(subject.name).to eq("test") }
     it { expect(Foo.find(subject.id)).to_not be_nil }
+  end
+
+  context "created Foo (lazy)" do
+    let!(:before_count) { Foo.count }
+    let(:foo) { Foo.create(:name=>"test") }
+
+    it { expect(foo).to be_persisted }
+    it { expect(foo.name).to eq("test") }
+    it { expect(Foo.find(foo.id)).to_not be_nil }
+    it { foo; expect(Foo.count).to eq(before_count + 1) }
   end
 end
