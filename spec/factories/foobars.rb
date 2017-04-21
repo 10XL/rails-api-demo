f2a38b690f201600a67ea27b224e2c14760ec02e
10xl <urnotuurmi@gmail.com>
2017-04-21 00:54:28 -0400

    FactoryGirl factories

--- spec/factories/foobars.rb
+++ spec/factories/foobars.rb
@@ -4,6 +4,32 @@ FactoryGirl.define do
     name "test"
   end
 
+  factory :foo_sequence, class: 'Foo' do
+    sequence(:name) { |n| "test#{n}" }
+  end
+
+  factory :foo_names, :parent=>:foo_fixed do
+    sequence(:name) { |n| ["larry", "moe", "curly"][n% 3] }
+  end
+
+  factory :foo_transient, class: 'Foo' do
+    name "test"
+    transient do
+      male true
+    end
+
+    after(:build) do |object, props|
+      object.name = props.male ? "Mr Test" : "Ms Test"
+    end
+  end
+
+  factory :foo_ctor, class: 'Foo' do
+    transient do
+      hash {}
+    end
+    initialize_with { Foo.new(hash) }
+  end
+
   factory :foo, :parent=>:foo_fixed do
   end
 end
