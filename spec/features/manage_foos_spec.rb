9cac415052eb204f15ad404471f9cabd4045c429
10xl <urnotuurmi@gmail.com>
2018-09-06 19:33:40 -0400

    foo ui helper module

--- spec/features/manage_foos_spec.rb
+++ spec/features/manage_foos_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
+require 'support/foo_ui_helper.rb'
 
 RSpec.feature "ManageFoos", type: :feature, :js=>true do
   include_context "db_cleanup_each"
-  FOO_FORM_XPATH = "//h3[text()='Foos']/../form"
-  FOO_LIST_XPATH = "//h3[text()='Foos']/../ul"
+  include FooUiHelper
+  FOO_FORM_XPATH = FooUiHelper::FOO_FORM_XPATH
+  FOO_LIST_XPATH = FooUiHelper::FOO_LIST_XPATH
+  let(:foo_state) { FactoryGirl.attributes_for(:foo) }
+
 
   feature "view existing Foos" do
     let(:foos) { (1..5).map { FactoryGirl.create(:foo) }.sort_by { |v| v["name"] } }
@@ -31,12 +35,85 @@ RSpec.feature "ManageFoos", type: :feature, :js=>true do
   end
 
   feature "add new Foo" do
-    scenario "has input form"
-    scenario "complete form"
+    background(:each) do
+      visit root_path
+      expect(page).to have_css("h3", text:"Foos")                 # on the Foos page
+      expect(page).to have_css("li", count:0)                     # nothing listed
+    end
+
+    scenario "has input form" do
+      expect(page).to have_css("label", :text=>"Name:")
+      expect(page).to have_css("input[name='name'][ng-model*='foo.name']")
+      expect(page).to have_css("button[ng-click*='create()']", :text=>"Create Foo")
+      expect(page).to have_button("Create Foo")
+    end
+
+    scenario "complete form" do
+      within(:xpath, FOO_FORM_XPATH) do
+        fill_in("name", :with=>foo_state[:name])
+        click_button("Create Foo")
+      end
+      within(:xpath, FOO_LIST_XPATH) do
+        expect(page).to have_css("li", count:1)
+        expect(page).to have_content(foo_state[:name])
+      end
+    end
+
+    scenario "complete form with XPath" do
+      # find(:xpath, "//input[@ng-model='foosVM.foo.name']").set(foo_state[:name])
+      find(:xpath, "//input[contains(@ng-model,'foo.name')]").set(foo_state[:name])
+      # find(:xpath, "//button[@ng-click='foosVM.create()']").click
+      find(:xpath, "//button[contains(@ng-click,'create()')]").click
+      within(:xpath, FOO_LIST_XPATH) do
+        expect(page).to have_xpath("//li", count:1)
+        # expect(page).to have_xpath("//*[text()='#{foo_state[:name]}']")
+        expect(page).to have_content(foo_state[:name])
+      end
+    end
+
+    scenario "complete form with helper" do
+      create_foo foo_state
+      within(:xpath, FOO_LIST_XPATH) do
+        expect(page).to have_css("li", count:1)
+      end
+    end
   end
 
   feature "with existing Foo" do
-    scenario "can be updated"
-    scenario "can be deleted"
+    background(:each) do
+      create_foo foo_state
+    end
+
+    scenario "can be updated" do
+      existing_name=foo_state[:name]
+      new_name=FactoryGirl.attributes_for(:foo)[:name]
+
+      within(:xpath,FOO_LIST_XPATH) do
+        expect(page).to have_css("li", :count=>1)
+        expect(page).to have_css("li", :text=>existing_name)
+        expect(page).to have_no_css("li", :text=>new_name)
+      end
+
+      update_foo(existing_name, new_name)
+
+      within(:xpath,FOO_LIST_XPATH) do
+        expect(page).to have_css("li", :count=>1)
+        expect(page).to have_no_css("li", :text=>existing_name)
+        expect(page).to have_css("li", :text=>new_name)
+      end
+    end
+
+    scenario "can be deleted" do
+      within(:xpath,FOO_LIST_XPATH) do
+        expect(page).to have_css("a",text:foo_state[:name])
+      end
+
+      delete_foo foo_state[:name]
+
+      within(:xpath,FOO_LIST_XPATH) do
+        expect(page).to have_no_css("a",text:foo_state[:name])
+      end
+    end
+
   end
 end
